08f43439cabdd30ccad1e17c2a3fb34a9c5db513
zeNOPE <nobel2545@gmail.com>
2024-02-19 23:08:20 +0700

    เพิ่มเติมนิดหน่อย เดี๋ยวแก้ต่อชัว

--- sketch.js
+++ sketch.js
@@ -4,4 +4,16 @@ function setup() {
   
   function draw() {
     background(25);
+    drawCloud();
+  }
+
+  function drawCloud(){
+    noStroke();
+    fill(random(250,255));
+    circle(50,150,50);
+    circle(85,140,75);
+    circle(100,100,70);
+    circle(140,110,50);
+    circle(120,145,80);
+    circle(160,140,60);
   }
